103c59c52054710e1693b89163143001b2c343a9
Latika <165281914+LatikaRai@users.noreply.github.com>
2025-04-04 18:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ btn.addEventListener('click', ()=>{
   //Choosing a Random Character Image
   let imgs = ['raven.png', 'star.png', 'beast.png', 'robin.png', 'cyborg.png']
   let i = Math.floor(Math.random()*imgs.length)
-  img.setAttribute('src',`./${imgs[i]}`)
+  img.setAttribute('src',`https://your-username.github.io/repository-name/${imgs[i]}`)
 
   //Setting image styles
   img.style.height = '30vh'
