5ed25ab3b546c137089219031c2b3f4f3885e031
Latika <165281914+LatikaRai@users.noreply.github.com>
2025-04-04 18:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//Selecting button element
+let btn = document.querySelector('button')
+
+//Adding an Event Listener
+btn.addEventListener('click', ()=>{
+  //Generating random values
+  let x = Math.random()*80 
+  let y = Math.random()*80 
+  let rot = Math.random()*360 
+
+  //Creating a random image element
+  let img = document.createElement('img')
+
+  //Choosing a Random Character Image
+  let imgs = ['raven.png', 'star.png', 'beast.png', 'robin.png', 'cyborg.png']
+  let i = Math.floor(Math.random()*imgs.length)
+  img.setAttribute('src',`/images/${imgs[i]}`)
+
+  //Setting image styles
+  img.style.height = '30vh'
+  img.style.position = 'absolute'
+  img.style.left = x+'%'
+  img.style.top = y+'%'
+  img.style.rotate = rot+'deg'
+
+  // Adding the image to the page
+  document.body.appendChild(img)
+})
